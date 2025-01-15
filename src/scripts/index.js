c7abaad5a172c0a23b028d37dc980f5228e08498
RailanTeles <135670809+RailanTeles@users.noreply.github.com>
2025-01-14 22:26:32 -0300

    NavBar feita

---
+++ src/scripts/index.js
@@ -0,0 +1,48 @@
+// Animação de observar
+const observers = document.querySelectorAll('.observer');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.5 
+});
+
+observers.forEach((element) => observer.observe(element));
+
+// NavBar
+const toggle = document.querySelector(".toggle");
+const navBar = document.querySelector(".NavBarMobile");
+const navBarCont = document.querySelector(".NavBarMobileContent");
+const toggleImg = document.querySelector("#toggleIMG");
+
+toggle.addEventListener('click', ()=>{
+    navBar.classList.toggle("OnNavBar");
+
+    
+    toggle.style.transition = "transform 0.7s ease"; 
+    toggle.style.transform = "rotate(360deg)"; 
+
+    setTimeout(() => {
+        toggle.style.transform = "rotate(0deg)"; 
+        if (navBar.classList.contains("OnNavBar")) {
+            toggleImg.src = "src/images/close.png"
+        } else {
+            toggleImg.src = "src/images/toggle.png"
+        }
+    }, 500); 
+});
+
+const itensNavBarMobile = document.querySelectorAll(".NavItensMobile");
+
+itensNavBarMobile.forEach(item => {
+    item.addEventListener('click', () => {
+        setTimeout(() => {
+            navBar.classList.remove("OnNavBar");
+            toggleImg.src = "src/images/toggle.png";
+        }, 500);
+    });
+});
