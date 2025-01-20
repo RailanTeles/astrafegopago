0c410de9b0186d2539eb5b50471e260093ea9606
RailanTeles <135670809+RailanTeles@users.noreply.github.com>
2025-01-20 00:15:38 -0300

    Teste bug

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -14,27 +14,27 @@ const observer = new IntersectionObserver((entries) => {
 observers.forEach((element) => observer.observe(element));
 
 // NavBar
-const toggle = document.querySelector(".toggle");
-const navBar = document.querySelector(".NavBarMobile");
-const navBarCont = document.querySelector(".NavBarMobileContent");
-const toggleImg = document.querySelector("#toggleIMG");
+// const toggle = document.querySelector(".toggle");
+// const navBar = document.querySelector(".NavBarMobile");
+// const navBarCont = document.querySelector(".NavBarMobileContent");
+// const toggleImg = document.querySelector("#toggleIMG");
 
-toggle.addEventListener('click', ()=>{
-    navBar.classList.toggle("OnNavBar");
+// toggle.addEventListener('click', ()=>{
+//     navBar.classList.toggle("OnNavBar");
 
     
-    toggle.style.transition = "transform 0.3s ease"; 
-    toggle.style.transform = "rotate(360deg)"; 
+//     toggle.style.transition = "transform 0.3s ease"; 
+//     toggle.style.transform = "rotate(360deg)"; 
 
-    setTimeout(() => {
-        toggle.style.transform = "rotate(0deg)"; 
-        if (navBar.classList.contains("OnNavBar")) {
-            toggleImg.src = "src/images/close.png"
-        } else {
-            toggleImg.src = "src/images/toggle.png"
-        }
-    }, 500); 
-});
+//     setTimeout(() => {
+//         toggle.style.transform = "rotate(0deg)"; 
+//         if (navBar.classList.contains("OnNavBar")) {
+//             toggleImg.src = "src/images/close.png"
+//         } else {
+//             toggleImg.src = "src/images/toggle.png"
+//         }
+//     }, 500); 
+// });
 
 const itensNavBarMobile = document.querySelectorAll(".NavItensMobile");
 
